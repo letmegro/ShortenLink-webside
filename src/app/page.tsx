9011ad90f22b6068049b8a6a47ed72c1f3d85925
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-11-07 00:07:56 -0500

    moved common nav html
    moved nav html into its own script in order to simplify customization of a common nav bar throughout the website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import fb from "public/images/icon-facebook.svg";
 import twitterX from "public/images/icon-twitter.svg";
 import pinterest from "public/images/icon-pinterest.svg";
 import instagram from "public/images/icon-instagram.svg";
+import Nav from './nav';
 //@ts-ignore
 import { setupListener } from './errMsg';
 import { createMobileNav } from "./toggleScript";
@@ -18,45 +19,9 @@ export default function Home() {
   useEffect(() => {createMobileNav(); setupListener();},[]);
   return (
     <main>
-      {/* nav */}
-      <nav className='relative container mx-auto p-6'>
-        {/* flex container */}
-        <div className='flex items-center justify-between'>
-          {/* logo container/menu */}
-          <div className='flex items-center space-x-20'>
-            <Image src={logo} alt=''/>
-            <div className='hidden md:flex space-x-8 font-bold'>
-              <a href='#' className='text-grayishViolet hover:text-veryDarkViolet'>Features</a>
-              <a href='#' className='text-grayishViolet hover:text-veryDarkViolet'>Pricing</a>
-              <a href='#' className='text-grayishViolet hover:text-veryDarkViolet'>Resources</a>
-            </div>
-          </div>
-          {/* right buttons menu */}
-          <div className='hidden md:flex items-center space-x-6 font-bold text-grayishViolet'>
-            <div className='hover:text-veryDarkViolet cursor-pointer'>
-              Login
-            </div>
-            <a href='#' className='px-8 py-3 font-bold text-white bg-cyan rounded-full hover:opacity-70'>Sign up</a>
-          </div>
-          <div className='md:hidden'>
-              <button id='menu-btn' type='button' className='z-40 block hamburger md:hidden focus:outline-none'>
-                <span className='hamTop'></span>
-                <span className='hamMiddle'></span>
-                <span className='hamBottom'></span>
-              </button>
-            </div>
-        </div>
-        {/* add drop menu */}
-        <div id="menu" className="absolute hidden md:hidden p-6 rounded-lg bg-darkViolet left-6 right-6 top-20 z-100">
-          <div className="flex flex-col items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
-            <a href="#" className="w-full text-center hover:text-cyan">Features</a>
-            <a href="#" className="w-full text-center hover:text-cyan">Pricing</a>
-            <a href="#" className="w-full text-center hover:text-cyan">Resources</a>
-            <a href="#" className="w-full pt-6 border-t border-gray-400 text-center hover:text-cyan">Login</a>
-            <a href="#" className="w-full py-3 text-center rounded-full bg-cyan hover:opacity-70">Sign Up</a>
-          </div>
-        </div>
-      </nav>
+      {/* nav bar */}
+      <Nav/>
+      {/* end of nav bar */}
       <section id='hero'>
         <div className='container flex flex-col-reverse mx-auto p-6 lg:flex-row'>
           {/* content */}
