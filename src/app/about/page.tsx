f193b46f52040d893b6cb6c4881fcb5ab61bb116
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-11-07 00:13:53 -0500

    finished refactoring
    Moved the nav and footer HTML into their own respective scripts in order to make editing of the nav and footer easier and to make the code cleaner.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,6 +9,7 @@ import instagram from "public/images/icon-instagram.svg";
 import hardAtWork from 'public/images/at-work.svg';
 import { createMobileNav } from "../toggleScript";
 import Nav from "../nav";
+import Footer from "../footer";
 
 export default function About(){
     useEffect(() => {
@@ -36,71 +37,8 @@ export default function About(){
         </main>
         
         {/* footer */}
-        <footer className="py-16 bg-veryDarkViolet">
-        <div className="container md:px-2 flex flex-col items-center justify-between mx-auto space-y-16 md:flex-row md:space-y-0 md:items-start">
-          <Image src={logo} alt=''/>
-
-          {/* menus container */}
-          <div className="flex flex-col space-y-16 md:space-x-20 md:flex-row md:space-y-0">
-            {/* menu 1 */}
-            <div className="flex flex-col items-center w-full md:items-start">
-              <div className="mb-5 font-bold text-white capitalize">Features</div>
-              <div className="flex flex-col items-center space-y-3 md:items-start">
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Link Shortening</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Branded Links</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Analytics</a>
-              </div>
-            </div>
-            {/* menu 2 */}
-            <div className="flex flex-col items-center w-full md:items-start">
-              <div className="mb-5 font-bold text-white capitalize">Resources</div>
-              <div className="flex flex-col items-center space-y-3 md:items-start">
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Blog</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Developers</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Support</a>
-              </div>
-            </div>
-            {/* menu 3 */}
-            <div className="flex flex-col items-center w-full md:items-start">
-              <div className="mb-5 font-bold text-white capitalize">Company</div>
-              <div className="flex flex-col items-center space-y-3 md:items-start">
-                <a href="/" className="capitalize text-grayishViolet hover:text-cyan">Home</a>
-                <a href='about' className="capitalize text-grayishViolet hover:text-cyan">About</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Careers</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Contact</a>
-              </div>
-            </div>
-          </div>
-          {/* socials */}
-          <div className="flex space-x-6">
-          
-                {/* icon 1 */}
-                <div className='h-8 group'>
-                  <a href='#'>
-                    <Image className='h-6 ficon' src={fb} alt=''></Image>
-                  </a>
-                </div>
-                {/* icon 2 */}
-                <div className='h-8 group'>
-                  <a href='#'>
-                    <Image className='h-6 ficon' src={instagram} alt=''></Image>
-                  </a>
-                </div>
-                {/* icon 3 */}
-                <div className='h-8 group'>
-                  <a href='#'>
-                    <Image className='h-6 ficon' src={pinterest} alt=''></Image>
-                  </a>
-                </div>
-                {/* icon 4 */}
-                <div className='h-7 ficon group bg-white'>
-                  <a href='#'>
-                    <Image className='h-8 ficon' src={twitterX} alt=''></Image>
-                  </a>
-                </div>
-          </div>
-        </div>
-      </footer>
+        <Footer/>
+        {/* end of footer tag :3 */}
     </>);
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client"
 import Image from 'next/image';
 import { useEffect } from 'react';
-import logo from 'public/images/logo.svg';
 import illustration from "public/images/illustration-working.svg";
 import iconBrandRec from "public/images/icon-brand-recognition.svg";
 import iconDetaledRec from "public/images/icon-detailed-records.svg";
 import iconFullyCus from "public/images/icon-fully-customizable.svg";
-import fb from "public/images/icon-facebook.svg";
-import twitterX from "public/images/icon-twitter.svg";
-import pinterest from "public/images/icon-pinterest.svg";
-import instagram from "public/images/icon-instagram.svg";
+
 import Nav from './nav';
+import Footer from './footer';
 //@ts-ignore
 import { setupListener } from './errMsg';
 import { createMobileNav } from "./toggleScript";
@@ -162,71 +159,11 @@ export default function Home() {
           </a>
         </div>
       </section>
-      <footer className="py-16 bg-veryDarkViolet">
-        <div className="container flex flex-col items-center justify-between mx-auto space-y-16 md:flex-row md:space-y-0 md:items-start">
-          <Image src={logo} alt=''/>
+      
+      {/* footer */}
+      <Footer/>
+      {/* end of footer */}
 
-          {/* menus container */}
-          <div className="flex flex-col space-y-16 md:space-x-20 md:flex-row md:space-y-0">
-            {/* menu 1 */}
-            <div className="flex flex-col items-center w-full md:items-start">
-              <div className="mb-5 font-bold text-white capitalize">Features</div>
-              <div className="flex flex-col items-center space-y-3 md:items-start">
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Link Shortening</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Branded Links</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Analytics</a>
-              </div>
-            </div>
-            {/* menu 2 */}
-            <div className="flex flex-col items-center w-full md:items-start">
-              <div className="mb-5 font-bold text-white capitalize">Resources</div>
-              <div className="flex flex-col items-center space-y-3 md:items-start">
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Blog</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Developers</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Support</a>
-              </div>
-            </div>
-            {/* menu 3 */}
-            <div className="flex flex-col items-center w-full md:items-start">
-              <div className="mb-5 font-bold text-white capitalize">Company</div>
-              <div className="flex flex-col items-center space-y-3 md:items-start">
-                <a href="/" className="capitalize text-grayishViolet hover:text-cyan">Home</a>
-                <a href='about' className="capitalize text-grayishViolet hover:text-cyan">About</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Careers</a>
-                <a href="#" className="capitalize text-grayishViolet hover:text-cyan">Contact</a>
-              </div>
-            </div>
-          </div>
-          {/* socials */}
-          <div className="flex space-x-6">
-          
-                {/* icon 1 */}
-                <div className='h-8 group'>
-                  <a href='#'>
-                    <Image className='h-6 ficon' src={fb} alt=''></Image>
-                  </a>
-                </div>
-                {/* icon 2 */}
-                <div className='h-8 group'>
-                  <a href='#'>
-                    <Image className='h-6 ficon' src={instagram} alt=''></Image>
-                  </a>
-                </div>
-                {/* icon 3 */}
-                <div className='h-8 group'>
-                  <a href='#'>
-                    <Image className='h-6 ficon' src={pinterest} alt=''></Image>
-                  </a>
-                </div>
-                {/* icon 4 */}
-                <div className='h-7 ficon group bg-white'>
-                  <a href='#'>
-                    <Image className='h-8 ficon' src={twitterX} alt=''></Image>
-                  </a>
-                </div>
-          </div>
-        </div>
-      </footer>
     </main>
   );
 }
