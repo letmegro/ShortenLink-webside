75407738e89099b4053c48ec1820ca70a198b027
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-11-13 19:54:34 -0500

    Revert "asd"
    This reverts commit e27f929d0565fecf3d26cbdf7aa4292e4501bfde.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -49,40 +49,37 @@ export default function About(){
             <section id="contact">
               <div className="m-2 flex flex-col md:flex-row mt-20 justify-between">
                 {/* map */}
-                <div className="container mx-auto p-2 md:w-1/2">
-                  <h1 className="select-none text-3xl md:text-4xl font-semibold text-center ">
+                <div className="container mx-auto p-2">
+                  <h1 className="select-none text-3xl md:text-4xl font-semibold text-center">
                     Company Locations
                   </h1>
                   <span className="line"></span>
-                  {/* map container */}
-                  <div className="container mx-auto max-w-full p-4">
-                    
+                  <div className="container mx-auto max-w-full">
+                    <h1 className="text-center">"Company location map to be added"</h1>
                   </div>
                 </div>
-                <div className="xl:w-1/2 m-12">
-                  {/* contact form */}
-                  <form className="xl:w-1/2 space-y-4 md:mt-12 shadow-xl shadow-zinc-400 bg-gray-50 border-solid border-4 p-2 rounded-lg border-blue-900 container mx-auto">
-                    <h1 className="select-none mb-2 text-3xl md:text-4xl font-semibold text-center">
-                      Contact Us
-                    </h1>
-                    <div className="flex flex-row space-x-2">
-                      {/* name inputs */}
-                      <input placeholder="First Name" type="text" className=" input-decor w-1/2  text-center" required/>
-                      <input placeholder="Last Name" type="text" className="input-decor w-1/2  text-center"  required/>
-                    </div>
-                    <input placeholder="Email" type="email" className="input-decor focus:scale-[1.02] w-full  text-center" required/>
-                    <textarea id="textArea" placeholder="Message..." className="custom-scroll-tArea overflow-y-auto w-full resize-none focus:scale-[1.02] h-32 input-decor text-ellipsis" />
-                    <div className="flex flex-row space-x-2">  
-                      <input type="checkbox" className="ml-12 cursor-pointer" required/>
-                      <p className="text-gray-400">By clicking the checkbox you agree to the terms and conditions</p>
-                      
-                    </div>
-                    <button type="submit" className="hover:bg-green-700 hover:shadow-lg shadow-lg transition duration-200 scale-105 shadow-red hover:shadow-green-600 flex mx-auto border-2  bg-orange-800 text-white border-black py-2 px-10 rounded-2xl">
-                      <p className="ml-7">Send</p>
-                      <Image alt="" src={eIcon} className="mail stroke-white w-[1.5rem] ml-5"></Image>
-                    </button>
-                  </form>
-                </div>
+                {/* contact form */}
+                <form className=" space-y-4 md:mt-12 shadow-lg shadow-zinc-300 bg-gray-50 border-solid border-2 p-2 rounded-lg border-blue-950 container mx-auto">
+                  <h1 className="select-none mb-2 text-3xl md:text-4xl font-semibold text-center">
+                    Contact Us
+                  </h1>
+                  <div className="flex flex-row space-x-2">
+                    {/* name inputs */}
+                    <input placeholder="First Name" type="text" className=" input-decor w-1/2  text-center" required/>
+                    <input placeholder="Last Name" type="text" className="input-decor w-1/2  text-center"  required/>
+                  </div>
+                  <input placeholder="Email" type="email" className="input-decor focus:scale-[1.02] w-full  text-center" required/>
+                  <textarea id="textArea" placeholder="Message..." className="custom-scroll-tArea overflow-y-auto w-full resize-none focus:scale-[1.02] h-32 input-decor text-ellipsis" />
+                  <div className="flex flex-row space-x-2">  
+                    <input type="checkbox" className="ml-12 cursor-pointer" required/>
+                    <p className="text-gray-400">By clicking the checkbox you agree to the terms and conditions</p>
+                    
+                  </div>
+                  <button type="submit" className="hover:bg-green-700 hover:shadow-lg shadow-lg transition duration-200 scale-105 shadow-red hover:shadow-green-600 flex mx-auto border-2  bg-orange-800 text-white border-black py-2 px-10 rounded-2xl">
+                    <p className="ml-7">Send</p>
+                    <Image alt="" src={eIcon} className="mail stroke-white w-[1.5rem] ml-5"></Image>
+                  </button>
+                </form>
               </div>
             </section>
 
